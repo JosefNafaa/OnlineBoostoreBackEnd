51c85571e59530f6f44733b002f39c2d6a6dfe5b
Yousef Nafaa <nafaa.youssef93@gmail.com>
2020-05-15 05:31:43 +0200

    commit initial

---
+++ src/main/java/com/example/onlineBoostoreBackEnd/OnlineBoostoreBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.example.onlineBoostoreBackEnd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineBoostoreBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineBoostoreBackEndApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/onlineBoostoreBackEnd/OnlineBoostoreBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.onlineBoostoreBackEnd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineBoostoreBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
